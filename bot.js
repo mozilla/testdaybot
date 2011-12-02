40598715a3f4ada0338444dd1ee3956ce34e31f8
AutomatedTester <dburns@mozilla.com>
2011-12-02 14:48:57 +0000

    updated so it doesnt spam people with bad connections

--- bot.js
+++ bot.js
@@ -2,13 +2,33 @@
 var irc = require('irc');
 
 var ircServer = 'irc.mozilla.org',
-    nick = '_TestDayBot',
-    options = {channels: ['#testday'],},
-    client = new irc.Client(ircServer, nick, options);
+    nick = '_TestDayBot3',
+    options = {channels: ['#tday'],autoRejoin: true,},
+    client = new irc.Client(ircServer, nick, options),
+    lastQuit = {};
 
 client.addListener('join', function(channel, who){
   if (who !== nick){
-    console.log("Greeted " + who);
-    client.say(channel, "Welcome to the Test Day " + who + "! Details of the Test Day can be found at https://etherpad.mozilla.org/testday-111202-webapps");
+    var lastMessageTime = Date.now() - lastQuit[who];
+    console.log(lastQuit.toString() + " time: " + lastMessageTime);
+    
+    if (lastQuit[who] && lastMessageTime < 1800000){
+      
+    } else {
+      console.log("Greeted " + who);
+      client.say(channel, "Welcome to the Test Day " + who + "! Details of the Test Day can be found at https://etherpad.mozilla.org/testday-111202-webapps");
+    }
   }
+});
+
+client.addListener('quit', function(who, reason, channel){
+  lastQuit[who] = Date.now();
+});
+
+client.addListener('part', function(channel, who, reason){
+  lastQuit[who] = Date.now();
+});
+
+client.addListener('error', function(message){
+  console.error(message);
 });
