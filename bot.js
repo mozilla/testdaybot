dddaedc1e1610c51bed21755283b1bcd74972df6
AutomatedTester <dburns@mozilla.com>
2012-01-20 09:34:53 +0000

    Updated for OpenWebApps Test Day

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/testday-20120113';
+    etherpad = 'https://etherpad.mozilla.org/testday-120120-webapps';
 
 client.addListener('join', function(channel, who){
   if (who !== nick){
