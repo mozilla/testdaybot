282114e8960ddac0ebd509fbc151389bfc02970e
AutomatedTester <dburns@mozilla.com>
2012-03-28 12:02:26 +0100

    updating etherpad

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/testday-20120323',
+    etherpad = 'https://etherpad.mozilla.org/testday-20120328',
     metrics = {
       greetedName: [],
       greetedNumber: 0,
