138f69b21dd9d6069bbc195ea80920bf4d9efe8a
AutomatedTester <dburns@mozilla.com>
2012-06-22 09:28:01 +0100

    Updated etherpad address... one day I will make this less manual

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/fx16-unconfirmed-bugs-testday',
+    etherpad = 'https://etherpad.mozilla.org/testday-20120622',
     metrics = {
       greetedName: [],
       greetedNumber: 0,
