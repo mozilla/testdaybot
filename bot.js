99227788f4988dd150e17a9a0898075b362ad5d1
AutomatedTester <dburns@mozilla.com>
2012-08-24 09:47:40 +0100

    apps test day

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/testday-20120817',
+    etherpad = 'https://etherpad.mozilla.org/testday-20120824-apps',
     metrics = {
       greetedName: [],
       greetedNumber: 0,
