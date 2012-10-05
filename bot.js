84b63c2a9fbe624e104a1987840e56a2a2f99ae5
AutomatedTester <dburns@mozilla.com>
2012-10-05 13:43:52 +0100

    aurora test day

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/testday-20120928',
+    etherpad = 'https://etherpad.mozilla.org/testday-20121005',
     metrics = {
       greetedName: [],
       greetedNumber: 0,
