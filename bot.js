52c41f9d1544ee919feecbabf1866fe0cef6aca2
AutomatedTester <dburns@mozilla.com>
2013-01-02 16:04:43 +0000

    added per-window private browsing day

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/automation-testday-20121214',
+    etherpad = 'https://etherpad.mozilla.org/testday-20130104',
     metrics = {
       greetedName: [],
       greetedNumber: 0,
