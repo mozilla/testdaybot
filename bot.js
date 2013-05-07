ec9c2842038622381edb4894cd45077c4b0f4d3d
AutomatedTester <dburns@mozilla.com>
2013-05-07 10:04:59 +0100

    Plugin test day

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/testday-20130412',
+    etherpad = 'https://etherpad.mozilla.org/testday-20130507',
     metrics = {
       greetedName: [],
       greetedNumber: 0,
