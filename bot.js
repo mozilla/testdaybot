734f419d5b9e54d70dae48037bde16063c822911
AutomatedTester <dburns@mozilla.com>
2013-05-24 10:39:19 +0100

    Fx 22b5 test day

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var ircServer = 'irc.mozilla.org',
     },
     client = new irc.Client(ircServer, nick, options),
     lastQuit = {},
-    etherpad = 'https://etherpad.mozilla.org/testday-20130507',
+    etherpad = 'https://etherpad.mozilla.org/testday-20130524',
     metrics = {
       greetedName: [],
       greetedNumber: 0,
