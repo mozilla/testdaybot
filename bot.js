b196246944322e9f6cf94acc2e8dcbaa50a33919
Barbara Miller <galgeek@me.com>
2014-12-18 01:30:12 -0800

    updates per issue 24 and associated bug fixes

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var irc = require('irc')
 var ircServer = 'irc.mozilla.org',
     nick = '_TestDayBot',
     options = {
-      channels: ['#testday'],
+      channels: ['#testdaybotTest'], // for testing; to be run in #qa?
       autoRejoin: true,
     },
     client = new irc.Client(ircServer, nick, options),
@@ -18,40 +18,69 @@ var ircServer = 'irc.mozilla.org',
       usersTalked: {},
       hourUTC: {},
     },
-    RUNNING_TIME = 1000 * 60 * 60 * 20;
+    testDay = false,
+    testDayAdmins = ["ashughes", "whimboo", "galgeek"],
+    helpers = ["ashughes"],
+    startTime = Date.now(),
+    endTime = startTime,
+    runTime = 0;
+
+function checkTestDay() {
+  if (testDay){
+    if (Date.now() > endTime){
+      testDay = false;
+    }
+  } else {
+    if (Date.now() < endTime && Date.now() > startTime){
+      testDay = true;
+      // re-initialize stats-related variables
+      lastQuit = {};
+      metrics = {
+        greetedName: [],
+        greetedNumber: 0,
+        firebotBugs:[],
+        usersTalked: {},
+        hourUTC: {},
+      };
+    }
+  }
+}
 
 client.addListener('join', function(channel, who){
-  if (who !== nick){
-    var lastMessageTime = Date.now() - lastQuit[who];
+  checkTestDay();
+  if (testDay){ // greet people only on test days
+    if (who !== nick){
+      var lastMessageTime = Date.now() - lastQuit[who];
 
-    if (lastQuit[who]){
-      switch (true){
-        case (lastMessageTime < 1800000):
-          break;
-        case (lastMessageTime < RUNNING_TIME):
-          setTimeout(function(){
-            client.say(channel, "Welcome back to the Test Day " + who + "!");
+      if (lastQuit[who]){
+        switch (true){
+          case (lastMessageTime < 1800000):
+            break;
+          case (lastMessageTime < runTime):
+            setTimeout(function(){
+              client.say(channel, "Welcome back to the Test Day " + who + "!");
+            }, 2000);
+            break;
+        }
+      } else {
+        console.log("Greeted " + who);
+        setTimeout(function(){
+          client.say(channel, "Welcome to the Test Day " + who + "! Details of the Test Day can be found at " + etherpad);
           }, 2000);
-          break;
+        metrics.greetedName.push(who);
+        metrics.greetedNumber +=1;
       }
-    } else {
-      console.log("Greeted " + who);
-      setTimeout(function(){
-        client.say(channel, "Welcome to the Test Day " + who + "! Details of the Test Day can be found at " + etherpad);
-        }, 2000);
-      metrics.greetedName.push(who);
-      metrics.greetedNumber +=1;
     }
   }
 });
 
 client.addListener('message', function(from, to, message){
+  checkTestDay();
+  if (message.search('[!:]help') >= 0){
+    client.say(to, "Commands I know:\n :bug\n :qmo\n :sumo\n :etherpad (test days)");
+  }
   if (message.search('[!:]bug') >= 0){
     client.say(to, "You can find details on how to raise a bug at https://developer.mozilla.org/en/Bug_writing_guidelines");
-  }
-
-  if (message.search('[!:]etherpad') >= 0){
-    client.say(to, "Today's etherpad is " + etherpad);
   }
   if (message.search('[!:]sumo') >= 0){
     client.say(to, "SUMO is short for http://support.mozilla.org, the official, community-powered support website for Mozilla Firefox");
@@ -59,94 +88,145 @@ client.addListener('message', function(from, to, message){
   if (message.search('[!:]qmo') >= 0){
     client.say(to, "QMO is short for http://quality.mozilla.org, the official destination for everything related with Mozilla QA");
   }
-  if (from === 'firebot'){
-    if (message.search(/https:\/\/bugzilla.mozilla.org\/show_bug.cgi\?id=(\d+)/i) >= 0){
-      metrics.firebotBugs.push(/https:\/\/bugzilla.mozilla.org\/show_bug.cgi\?id=(\d+)/i.exec(message)[1]);
+  if (testDay){
+    if (message.search('[!:]etherpad') >= 0){
+      client.say(to, "Today's etherpad is " + etherpad);
     }
-  }
-  if (from in metrics.usersTalked) {
-    metrics.usersTalked[from] += 1;
-  } else {
-    metrics.usersTalked[from] = 1;
-  }
-  var nowHour = new Date().getUTCHours().toString();
-  if (nowHour in metrics.hourUTC) {
-    metrics.hourUTC[nowHour] += 1;
-  } else {
-    metrics.hourUTC[nowHour] = 1;
+    if (from === 'firebot'){
+      if (message.search(/https:\/\/bugzilla.mozilla.org\/show_bug.cgi\?id=(\d+)/i) >= 0){
+        metrics.firebotBugs.push(/https:\/\/bugzilla.mozilla.org\/show_bug.cgi\?id=(\d+)/i.exec(message)[1]);
+      }
+    }
+    if (from in metrics.usersTalked) {
+      metrics.usersTalked[from] += 1;
+    } else {
+      metrics.usersTalked[from] = 1;
+    }
+    var nowHour = new Date().getUTCHours().toString();
+    if (nowHour in metrics.hourUTC) {
+      metrics.hourUTC[nowHour] += 1;
+    } else {
+      metrics.hourUTC[nowHour] = 1;
+    }
   }
 });
 
-client.addListener('pm', function(nick, message){
-  if (message.search('stats') === 0){
-    var stats = new Stats();
-    stats.generateStats(metrics);
+client.addListener('pm', function(from, message){ // private messages to bot
+  checkTestDay();
+  if (testDayAdmins.indexOf(from) >= 0){
+    if (message.search(':adminhelp') === 0){
+      client.say(from, "admin commands:\n  :addAdmin <nickname>\n  :addHelper <nickname>\n" + 
+                       "  :next <start as YYYY-MM-DDThh:mmZ> <end as YYYY-MM-DDThh:mmZ> <etherpad> <topic>\n" + 
+                       "  :start <etherpad> <topic>\n  :stats\n  :stop");
+      return;
+    }
+    if (message.search(':addAdmin') === 0){
+      addTestDayAdmin = message.slice(message.indexOf(" ") + 1);
+      testDayAdmins.push(addTestDayAdmin);
+      client.say(from, 'test day admins are now ' + testDayAdmins.toString());
+      return;
+    }
+    if (message.search(':addHelper') === 0){
+      addHelper = message.slice(message.indexOf(" ") + 1);
+      helpers.push(addHelper);
+      client.say(from, 'test day helpers are now ' + helpers.toString());
+      return;
+    }
+    if (message.search(':stats') === 0){
+      var stats = new Stats();
+      stats.generateStats(metrics, from);
+      return;
+    }
+    if (testDay){
+      if (message.search(':stop') === 0){
+        testDay = false;
+        client.say(from, "testDay is now " + testDay.toString());
+        return;
+      }
+    } else {
+      if (message.search(':start') === 0){
+        testDay = true;
+        args = message.slice(message.indexOf(" ") + 1);
+        etherpad = args.slice(0, args.indexOf(" "));
+        topic = args.slice(args.indexOf(" ") + 1);
+        startTime = Date.now();
+        runTime = 1000 * 60 * 60 * 20;  // default: 20 hours
+        endTime = startTime + runTime;
+        // re-initialize stats-related variables
+        lastQuit = {};
+        metrics = {
+          greetedName: [],
+          greetedNumber: 0,
+          firebotBugs:[],
+          usersTalked: {},
+          hourUTC: {},
+        };
+        // TODO: other initialization?
+        client.say(from, "testDay is now " + testDay.toString());
+        client.say(from, "Today's etherpad is " + etherpad);
+        client.say(from, "Today's topic is " + topic);
+        return;
+      }
+      if (message.search(':next') === 0){
+        args = message.slice(message.indexOf(" ") + 1);
+        startTime = new Date(args.slice(0, args.indexOf(" ")));
+        args = args.slice(args.indexOf(" ") + 1);
+        endTime = new Date(args.slice(0, args.indexOf(" ")));
+        args = args.slice(args.indexOf(" ") + 1);
+        etherpad = args.slice(0, args.indexOf(" "));
+        topic = args.slice(args.indexOf(" ") + 1);
+        client.say(from, "Next test day's start is " + startTime);
+        client.say(from, "Next test day's end is " + endTime);
+        client.say(from, "Next test day's etherpad is " + etherpad);
+        client.say(from, "Next test day's topic is " + topic);
+      }
+    }
   }
 });
 
 client.addListener('quit', function(who, reason, channel){
-  lastQuit[who] = Date.now();
+  checkTestDay();
+  if (testDay){
+    lastQuit[who] = Date.now();
+  }
 });
 
 client.addListener('part', function(channel, who, reason){
-  lastQuit[who] = Date.now();
+  checkTestDay();
+  if (testDay){
+    lastQuit[who] = Date.now();
+  }
 });
 
 client.addListener('error', function(message){
   console.error(message);
 });
 
-setTimeout(function(){
-    var stats = new Stats();
-    stats.generateStats(metrics, function() {
-      process.exit();
-    });
-  }, RUNNING_TIME);
-
 var Stats = function(){};
 
-Stats.prototype.generateStats = function(metrcs, callback){
+Stats.prototype.generateStats = function(metrcs, from){
   metrcs.testday = etherpad;
-  var options = {
-    host: 'testdayserver.appspot.com',
-    port: 80,
-    path: '/bot',
-    method: 'POST',
-    headers: {
-      'Content-length': JSON.stringify(metrcs).length,
-    }
-  };
-  var req = http.request(options, function(res){
-    console.log("REQUEST SENT TO APPENGINE");
-    callback();
-  });
-  req.on('error', function(e){
-    console.error(e);
-  });
-
-  req.write(JSON.stringify(metrcs));
-  req.end();
 
   var keys = Object.keys(metrcs);
   var what = Object.prototype.toString;
   for (var i = 0; i < keys.length; i++){
     if (what.call(metrcs[keys[i]]).search('Array') > 0){
-      console.log(keys[i] + ":  " + metrcs[keys[i]].join(", "));
+      client.say(from, keys[i] + ":  " + metrcs[keys[i]].join(", "));
     } else {
       if (keys[i] == "usersTalked"){
-        console.log("The following people were active in the channel: ");
+        client.say(from, "The following people were active in the channel: ");
         var speakers = Object.keys(metrcs.usersTalked);
         for (var t = 0; t < speakers.length; t++){
-          console.log(speakers[t] + ": " + metrcs.usersTalked[speakers[t]]);
+          client.say(from, speakers[t] + ": " + metrcs.usersTalked[speakers[t]]);
         }
       } else if (keys[i] == "hourUTC") {
-        console.log("The following hours were active in the channel: ");
+        client.say(from, "The following hours were active in the channel: ");
         var speakers = Object.keys(metrcs.hourUTC);
         for (var t = 0; t < speakers.length; t++){
-          console.log(speakers[t] + ": " + metrcs.hourUTC[speakers[t]]);
+          client.say(from, speakers[t] + ": " + metrcs.hourUTC[speakers[t]]);
         }
       } else {
-        console.log(keys[i] + ": " + metrcs[keys[i]]);
+        client.say(from, keys[i] + ": " + metrcs[keys[i]]);
       }
     }
   }
